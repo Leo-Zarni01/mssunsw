ca3f186d2d1d4b0b7ec4648331e182f1fdb74b54
Chaw Su Thwe <chawsuthwe181@gmail.com>
2025-01-26 17:38:23 +1100

    feat: draft layout

--- frontend/src/PageList.tsx
+++ frontend/src/PageList.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage.tsx';
+import UpcomingEventDetails from './components/UpcomingEventDetails.tsx';
 
 const PageList = () => {
     return (
         <Routes>
             <Route path='/' element={<HomePage />} />
+            <Route path='/events' element={<UpcomingEventDetails />} /> {/* /events/:id */}
+            
         </Routes>
     )
 }
