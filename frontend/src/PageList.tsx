dadedb3e6765fdd96a46f7cc123e69735add4f50
acs237 <acs23072000@gmail.com>
2025-01-27 23:16:09 +1100

    first version of ourstory page

--- frontend/src/PageList.tsx
+++ frontend/src/PageList.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage.tsx';
+import OurStory from './components/OurStory.tsx';
 
 const PageList = () => {
     return (
         <Routes>
             <Route path='/' element={<HomePage />} />
+            <Route path='/ourstory' element={<OurStory />} />
         </Routes>
     )
 }
