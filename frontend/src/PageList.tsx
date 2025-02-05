b3b909b580b35f61c6b0deb443e2f4df57348ecd
acs237 <acs23072000@gmail.com>
2025-02-05 16:40:02 +1100

    past event photo gallery and photo preview

--- frontend/src/PageList.tsx
+++ frontend/src/PageList.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage.tsx';
 import OurStory from './components/OurStory.tsx';
 import UpcomingEventDetails from './components/UpcomingEventDetails.tsx';
+import PastEventPhotoGallery from './components/PastEventPhotoGallery.tsx'
 
 const PageList = () => {
     return (
@@ -9,6 +10,7 @@ const PageList = () => {
             <Route path='/' element={<HomePage />} />
             <Route path='/ourstory' element={<OurStory />} />
             <Route path='/events' element={<UpcomingEventDetails />} /> {/* /events/:id */}
+            <Route path='/events/:eventId/photos' element={<PastEventPhotoGallery />} /> {/* /events/:id/photos */}
             
         </Routes>
     )
